83d3eca26fcb0d90c503a03f3223f3f4413e3276
Jonas <jonha860@gmail.com>
2014-04-10 17:47:21 +0200

    Tried to make robot walk, something is wrong with inverse kintematics

--- Styrmodul/Gang/highLevelWalking/highLevelWalking/highLevelWalking/highLevelWalking.c
+++ Styrmodul/Gang/highLevelWalking/highLevelWalking/highLevelWalking/highLevelWalking.c
@@ -20,34 +20,18 @@
 #define centerToSideLegs 100
 #define centerToFrontLegs 135
 #define centerToFrontLegsX 61.85
-int x0_1 = -200; //standard x pos for leg 1
-int y0_1 = 220; //standard y pos for leg 1
-int x0_2 = -180; //standard x pos for leg 2
+int x0_1 = -240; //standard x pos for leg 1
+int y0_1 = 250; //standard y pos for leg 1
+int x0_2 = -250; //standard x pos for leg 2
 int y0_2 = 0; //standard y pos for leg 2
-int x0_3 = -200; //standard x pos for leg 3
-int y0_3 = -220; //standard y pos for leg 3
-int x0_4 = 200; //standard x pos for leg 4
-int y0_4 = -220; //standard y pos for leg 4
-int x0_5 = 180; //standard x pos for leg 5
+int x0_3 = -240; //standard x pos for leg 3
+int y0_3 = -250; //standard y pos for leg 3
+int x0_4 = 240; //standard x pos for leg 4
+int y0_4 = -250; //standard y pos for leg 4
+int x0_5 = 250; //standard x pos for leg 5
 int y0_5 = 0; //standard y pos for leg 5
-int x0_6 = -200; //standard x pos for leg 6
-int y0_6 = 220; //standard y pos for leg 6
-
-	
-void moveLeg2too(int x,int y,int z,int speed)
-{}
-
-void moveLeg3too(int x,int y,int z,int speed)
-{}
-
-void moveLeg4too(int x,int y,int z,int speed)
-{}
-
-void moveLeg5too(int x,int y,int z,int speed)
-{}
-
-void moveLeg6too(int x,int y,int z,int speed)
-{}
+int x0_6 = -240; //standard x pos for leg 6
+int y0_6 = 250; //standard y pos for leg 6
 
 void moveRobot(int direction,int distance, int rotation, int z, int servoSpeed, int rotationX, int rotationY)
 {
@@ -59,24 +43,24 @@ void moveRobot(int direction,int distance, int rotation, int z, int servoSpeed, 
 	double sinaroundy = cos(rotationY);
 	
 	//First state-------------------
-	servoGoto(4, 3.1415/4, servoSpeed); //raise legs
-	servoGoto(10, 3.1415/4, servoSpeed);
-	servoGoto(15, -3.1415/4, servoSpeed);
-	_delay_ms(50);
+	servoGoto(4, 3.1415/2, servoSpeed); //raise legs
+	servoGoto(10, 3.1415/2, servoSpeed);
+	servoGoto(15, -3.1415/2, servoSpeed);
+	_delay_ms(1000);
 	moveLeg1too(x0_1*cosrotation-y0_1*sinrotation-sindirection*distance, y0_1*cosrotation-x0_1*sinrotation+cosdirection*distance, z+sinaroundy*centerToFrontLegsX+sinaroundx*centerToFrontLegsY, servoSpeed);
 	moveLeg3too(x0_3*cosrotation-y0_3*sinrotation-sindirection*distance, y0_3*cosrotation-x0_3*sinrotation+cosdirection*distance, z+sinaroundy*centerToFrontLegsX-sinaroundx*centerToFrontLegsY, servoSpeed);
 	moveLeg5too(x0_5*cosrotation-y0_5*sinrotation-sindirection*distance, y0_5*cosrotation+x0_5*sinrotation+cosdirection*distance, z-centerToSideLegs*sinaroundy, servoSpeed);
-	_delay_ms(1000);
+	_delay_ms(3000);
 		
 	//Second state-------------------
-	servoGoto(3, -3.1415/4, servoSpeed); //raise legs
-	servoGoto(9, -3.1415/4, servoSpeed);
-	servoGoto(16, 3.1415/4, servoSpeed);
-	_delay_ms(50);
+	servoGoto(3, -3.1415/2, servoSpeed); //raise legs
+	servoGoto(9, -3.1415/2, servoSpeed);
+	servoGoto(16, 3.1415/2, servoSpeed);
+	_delay_ms(1000);
 	moveLeg2too(x0_2*cosrotation-y0_2*sinrotation-sindirection*distance, y0_2*cosrotation+x0_2*sinrotation+cosdirection*distance, z+centerToSideLegs*sinaroundy, servoSpeed);
 	moveLeg4too(x0_4*cosrotation-y0_4*sinrotation-sindirection*distance, y0_4*cosrotation-x0_4*sinrotation+cosdirection*distance, z-sinaroundy*centerToFrontLegsX-sinaroundx*centerToFrontLegsY, servoSpeed);
 	moveLeg6too(x0_6*cosrotation-y0_6*sinrotation-sindirection*distance, y0_6*cosrotation-x0_6*sinrotation+cosdirection*distance, z-sinaroundy*centerToFrontLegsX+sinaroundx*centerToFrontLegsY, servoSpeed);
-	_delay_ms(1000);
+	_delay_ms(3000);
 		
 	//Third state-------------------
 	moveLeg1too(x0_1, y0_1, z+sinaroundy*centerToFrontLegsX+sinaroundx*centerToFrontLegsY, servoSpeed);
@@ -93,14 +77,59 @@ int main(void)
 	DDRD |= (1<<PORTD5); //init LED
 	//servoTx;
 	
+	servoRetrunLevel(BROADCASTING_ID, 1); //turns off return packets
+	
 	sei();
 	initServoSerial(); //Init servos
 	USART_init();
 	
+	/*double alpha = 3.1415/4;
+	double beta = 3.1415/2.2;
+	int speed = 180;
+	
+	//dummy code that puts robot in standard position
+	
+	servoGoto(1, 0, speed);
+	servoGoto(2, 0, speed);
+	servoGoto(7, 0, speed);
+	servoGoto(8, 0, speed);
+	servoGoto(13, 0, speed);
+	servoGoto(14, 0, speed);
+	
+	_delay_ms(100);
+	
+	servoGoto(15, -alpha, speed);
+	servoGoto(3, -alpha, speed);
+	servoGoto(9, -alpha, speed);
+	servoGoto(16, alpha, speed);
+	servoGoto(4, alpha, speed);
+	servoGoto(10, alpha, speed);
+	
+	_delay_ms(100);
+	
+	servoGoto(12, -beta, speed);
+	servoGoto(18, -beta, speed);
+	servoGoto(6, -beta, speed);
+	servoGoto(5, beta, speed);
+	servoGoto(11, beta, speed);
+	servoGoto(17, beta, speed);
+	
+	_delay_ms(5000);*/
+	
+	moveLeg1too(x0_1, y0_1, 70, 80);
+	moveLeg2too(x0_2, y0_2, 70, 80);
+	moveLeg3too(x0_3, y0_3, 70, 80);
+	moveLeg4too(x0_4, y0_4, 70, 80);
+	moveLeg5too(x0_5, y0_5, 70, 80);
+	moveLeg6too(x0_6, y0_6, 70, 80);
+	
+	_delay_ms(10000);
+	
     while(1)
     {
 		USART_DecodeRxFIFO();
-		_delay_ms(1000);
+		_delay_ms(3000);
+		moveRobot(0,25,0,70,100,0,0);
         //TODO:: Please write your application code 
     }
 }
--- Styrmodul/Gang/highLevelWalking/highLevelWalking/highLevelWalking/inverseKinematics.c
+++ Styrmodul/Gang/highLevelWalking/highLevelWalking/highLevelWalking/inverseKinematics.c
@@ -25,7 +25,15 @@
 double sinAngleToFrontLegs = centerToFrontLegsY / centerToFrontLegs;
 double cosAngleToFrontLegs = centerToFrontLegsX / centerToFrontLegs;
 
-void LegOneGoto(double x,double y,double z, int servospeed)
+void LegGoto(double x,double y,double z, int servospeed, int side, int servo1, int servo2, int servo3);
+void moveLeg1too(double x, double y, double z, int servospeed);
+void moveLeg2too(double x, double y, double z, int servospeed);
+void moveLeg3too(double x, double y, double z, int servospeed);
+void moveLeg4too(double x, double y, double z, int servospeed);
+void moveLeg5too(double x, double y, double z, int servospeed);
+void moveLeg6too(double x, double y, double z, int servospeed);
+
+void LegGoto(double x,double y,double z, int servospeed, int side, int servo1, int servo2, int servo3)
 {
 	double alpha;
 	double beta;
@@ -37,17 +45,51 @@ void LegOneGoto(double x,double y,double z, int servospeed)
 	beta = 3.1415 - acos((femur*femur+tibia*tibia-d*d)/(2*femur*tibia));
 	alpha = acos((femur*femur-tibia*tibia+d*d)/(2*femur*d))-asin(fabs(z)/d);
 	
-	servoGoto(8, gamma, servospeed);
-	servoGoto(10, alpha + femurAngleAddition,servospeed);
-	servoGoto(12, -beta + tibiaAngleAddition,servospeed);
+	servoGoto(servo1, gamma, servospeed);
+	servoGoto(servo2, side*(alpha + femurAngleAddition),servospeed);
+	servoGoto(servo3, side*(-beta + tibiaAngleAddition),servospeed);
 }
 
 void moveLeg1too(double x, double y, double z, int servospeed) //Help function to describe position of leg in standard base x,y,z
 {
 	double a = (x + centerToFrontLegsX)*cos(-3*3.1415/4)-(y-centerToFrontLegsY)*sin(-3*3.1415/4);
 	double b = (x + centerToFrontLegsX)*sin(-3*3.1415/4)+(y-centerToFrontLegsY)*cos(-3*3.1415/4);
-	LegOneGoto(a, b, z, servospeed);
+	LegGoto(a, b, z, servospeed,1,8,10,12);
 }
 
+void moveLeg2too(double x, double y, double z, int servospeed) //Help function to describe position of leg in standard base x,y,z
+{
+	double a = (x + centerToSideLegs)*cos(-3.1415)-(y)*sin(-3.1415);
+	double b = (x + centerToSideLegs)*sin(-3.1415)+(y)*cos(-3.1415);
+	LegGoto(a, b, z, servospeed,1,14,16,18);
+}
+
+void moveLeg3too(double x, double y, double z, int servospeed) //Help function to describe position of leg in standard base x,y,z
+{
+	double a = (x + centerToFrontLegsX)*cos(-5*3.1415/4)-(y+centerToFrontLegsY)*sin(-5*3.1415/4);
+	double b = (x + centerToFrontLegsX)*sin(-5*3.1415/4)+(y+centerToFrontLegsY)*cos(-5*3.1415/4);
+	LegGoto(a, b, z, servospeed,1,2,4,6);
+}
+
+void moveLeg4too(double x, double y, double z, int servospeed) //Help function to describe position of leg in standard base x,y,z
+{
+	double a = (x - centerToFrontLegsX)*cos(3.1415/4)-(y+centerToFrontLegsY)*sin(3.1415/4);
+	double b = (x - centerToFrontLegsX)*sin(3.1415/4)+(y+centerToFrontLegsY)*cos(3.1415/4);
+	LegGoto(a, b, z, servospeed,-1,1,3,5);
+}
+
+void moveLeg5too(double x, double y, double z, int servospeed) //Help function to describe position of leg in standard base x,y,z
+{
+	double a = (x - centerToSideLegs)*cos(0)-(y)*sin(0);
+	double b = (x - centerToSideLegs)*sin(0)+(y)*cos(0);
+	LegGoto(a, b, z, servospeed,-1,13,15,17);
+}
+
+void moveLeg6too(double x, double y, double z, int servospeed) //Help function to describe position of leg in standard base x,y,z
+{
+	double a = (x - centerToFrontLegsX)*cos(-3.1415/4)-(y-centerToFrontLegsY)*sin(-3.1415/4);
+	double b = (x - centerToFrontLegsX)*sin(-3.1415/4)+(y-centerToFrontLegsY)*cos(-3.1415/4);
+	LegGoto(a, b, z, servospeed,-1,7,9,11);
+}
 
 
